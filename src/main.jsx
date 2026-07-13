7e067a9cf8aeb1b7aaf209b375a5803e9a056d4d
Mohammed Esmael <mamiesmael391@gmail.com>
2026-07-12 23:14:28 -0400

    imported react-router-dom and added the required components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import './App.css'
 
+// BrowserRouter must wrap the whole app so any component can use routing.
+// It listens to the URL and hands control of what renders to React Router.
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
